2427304c1c51fef7df83bd76ca1a2ad7e494787f
wangyangyang <769789170@qq.com>
2020-04-19 15:05:32 +0800

    add icon

--- muziko/AppDelegate.swift
+++ muziko/AppDelegate.swift
@@ -10,17 +10,99 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
+    
+    @IBOutlet weak var Menu: NSMenu!
+    
+    
+    // 创建状态栏按钮
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+       // applicationDidFinishLaunching生命周期
+        
+            if let button = statusItem.button {
+                 button.image = NSImage(named: "StatusIcon")
+               
+            }
+        if let button = statusItem.button {
+                button.image = NSImage(named: "StatusIcon")
+                button.action = #selector(togglePopover(_:))
+               // 点击事件
+               button.action = #selector(mouseClickHandler)
+               button.sendAction(on: [.leftMouseUp, .rightMouseUp])
+        }
+        popover.contentViewController =            PopoverViewController.freshController()
+        
+        eventMonitor = EventMonitor(mask: [.leftMouseDown, .rightMouseDown]) { [weak self] event in
+          if let strongSelf = self, strongSelf.popover.isShown {
+            strongSelf.closePopover(event!)
+          }
+        }
+
+        // 修复按钮单击事件无效问题
+        Menu.delegate = self
+
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
+    
+    // 声明一个Popover
+    let popover = NSPopover()
+    
+    // 控制Popover状态
+        @objc func togglePopover(_ sender: AnyObject) {
+            if popover.isShown {
+                closePopover(sender)
+            } else {
+                showPopover(sender)
+            }
+        }
+       // 显示Popover
+        @objc func showPopover(_ sender: AnyObject) {
+            if let button = statusItem.button {
+                popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+            }
+            eventMonitor?.start()
+            
+        }
+        // 隐藏Popover
+        @objc func closePopover(_ sender: AnyObject) {
+            popover.performClose(sender)
+            eventMonitor?.stop()
+        }
 
+    // 关闭app
+    @IBAction func Quit(_ sender: Any) {
+        NSApplication.shared.terminate(self)
+    }
+    
+    // 声明监视器
+    var eventMonitor: EventMonitor?
+    
+    // 接管togglePopover
+    @objc func mouseClickHandler() {
+        if let event = NSApp.currentEvent {
+            switch event.type {
+            case .leftMouseUp:
+                togglePopover(popover)
+            default:
+                statusItem.menu = Menu
+                statusItem.button?.performClick(nil)
+                
+            }
+        }
+    }
 
+    
+}
+
+extension AppDelegate: NSMenuDelegate {
+    // 为了保证按钮的单击事件设置有效，menu要去除
+    func menuDidClose(_ menu: NSMenu) {
+        
+        self.statusItem.menu = nil
+    }
 }
 
--- muziko/PopoverViewController.swift
+++ muziko/PopoverViewController.swift
@@ -7,12 +7,97 @@
 //
 
 import Cocoa
+import Alamofire
+import SwiftyJSON
 
 class PopoverViewController: NSViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do view setup here.
-    }
     
-}
+    @IBOutlet weak var City: NSTextField!
+    
+    @IBOutlet weak var cond_txt_d: NSTextField!
+    
+    @IBOutlet weak var tmp_min: NSTextField!
+    @IBOutlet weak var tmp_max: NSTextField!
+    
+    private var timer: Timer?
+        // 定时器记数: 每20分钟执行一次，3轮为1小时
+        private var timeCount = 3
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // 获取并设置页面数据
+            setPageData()
+            // 启动定时器
+            loop()
+        }
+        
+        // 获取并设置数据
+        func setPageData(){
+            // 发起post请求
+            AF.request("https://free-api.heweather.net/s6/weather/forecast?location=jiaxing&key=15aab63b5ef44ecf95659ca16b4b0d3f",
+                       method: .get,encoding: JSONEncoding.default).responseJSON { (response) in
+                        print("\(response)")
+                        
+                switch response.result {
+                // 请求成功
+                case .success(let resData):
+                    // 将返回的数据转为JSON对象
+                    let jsonData = JSON.init(resData as Any)
+                    // 变量赋值
+                    self.City.stringValue = jsonData["HeWeather6"][0]["basic"]["location"].string!
+                    self.cond_txt_d.stringValue = jsonData["HeWeather6"][0]["daily_forecast"][0]["cond_txt_d"].string!
+                    self.tmp_min.stringValue = jsonData["HeWeather6"][0]["daily_forecast"][0]["tmp_min"].string!
+                    self.tmp_max.stringValue = jsonData["HeWeather6"][0]["daily_forecast"][0]["tmp_max"].string!
+
+                    
+                    break
+                case .failure(let error):
+                    print("接口调用失败")
+                    print(error);
+                    break
+                }
+            }
+        }
+        
+        // GCD 方式的定时器，循环【】
+        func loop() {
+            print("\(Date()): 定时器初始化")
+            // timeInterval: 隔多少秒执行一次
+            timer = Timer(timeInterval: 1200, repeats: true, block: { timer in
+                self.loopFireHandler(timer)
+            })
+            // 添加定时器
+            RunLoop.main.add(timer!, forMode: .common)
+        }
+        // 定时器需要执行的内容
+        @objc private func loopFireHandler(_ timer: Timer?) -> Void {
+            // 定时器执行结束结束
+            if self.timeCount <= 0 {
+                print("\(Date()): 执行完1轮，开始下一轮")
+                self.timeCount = 3
+                return
+            }
+            // 获取并设置页面数据
+            setPageData()
+            // 执行完分钟
+            self.timeCount -= 1
+          
+        }
+
+    }
+
+
+    extension PopoverViewController {
+        static func freshController() -> PopoverViewController {
+            //获取对Main.storyboard的引用
+            let storyboard = NSStoryboard(name: NSStoryboard.Name("Main"), bundle: nil)
+            // 为PopoverViewController创建一个标识符
+            let identifier = NSStoryboard.SceneIdentifier("PopoverViewController")
+            // 实例化PopoverViewController并返回
+            guard let viewcontroller = storyboard.instantiateController(withIdentifier: identifier) as? PopoverViewController else {
+                fatalError("Something Wrong with Main.storyboard")
+            }
+            return viewcontroller
+        }
+    }
