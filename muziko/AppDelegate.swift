4ed07dd418c087d7bd8b3a1dc1886b374ed95f9a
wangyangyang <769789170@qq.com>
2020-04-19 10:11:58 +0800

    Initial Commit

---
+++ muziko/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  muziko
+//
+//  Created by 王洋 on 2020/4/19.
+//  Copyright © 2020 王洋. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ muziko/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  muziko
+//
+//  Created by 王洋 on 2020/4/19.
+//  Copyright © 2020 王洋. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
