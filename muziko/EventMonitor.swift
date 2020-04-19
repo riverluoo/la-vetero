0995728802f132c45ae7a39f0bf7c2443e87e9e5
wangyangyang <769789170@qq.com>
2020-04-19 15:05:16 +0800

    add icon

---
+++ muziko/EventMonitor.swift
@@ -0,0 +1,9 @@
+//
+//  EventMonitor.swift
+//  muziko
+//
+//  Created by 王洋 on 2020/4/19.
+//  Copyright © 2020 王洋. All rights reserved.
+//
+
+import Foundation
---
+++ muziko/PopoverViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PopoverViewController.swift
+//  muziko
+//
+//  Created by 王洋 on 2020/4/19.
+//  Copyright © 2020 王洋. All rights reserved.
+//
+
+import Cocoa
+
+class PopoverViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
